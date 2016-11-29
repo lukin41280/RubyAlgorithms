774db06a6b5127b38e618f56d4dc62282aedd526
lukin41280 <jpelletier19@me.com>
2016-11-29 16:56:40 -0500

    first test passing

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,1 +1,13 @@
-fixed_array_spec.rb
+require 'rspec'
+require_relative 'fixed_array'
+
+describe FixedArray do
+  
+
+  	it "creates a new FixedArray" do
+  		array = FixedArray.new(3)
+  		expect(array.new_array).to eq [nil, nil, nil]
+  	end
+
+
+end
