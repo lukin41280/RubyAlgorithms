09b3733940bff54093c62b48a054fa944331dc51
lukin41280 <jpelletier19@me.com>
2016-11-30 10:24:30 -0500

    tests written and all pass

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -3,11 +3,38 @@ require_relative 'fixed_array'
 
 describe FixedArray do
   
+  	let(:array) {FixedArray.new(3)}
 
-  	it "creates a new FixedArray" do
-  		array = FixedArray.new(3)
-  		expect(array.new_array).to eq [nil, nil, nil]
+	describe "initialize" do
+	  	it "creates a new FixedArray" do
+	  		expect(array.new_array).to eq [nil, nil, nil]
+	  	end
+
+	  	it "throws error when no attribute given" do
+	  		expect {FixedArray.new}.to raise_error(ArgumentError)
+	  	end
   	end
 
+  	describe "#set" do
+  		it "updates index with provided element" do
+  			array.set(1, "cat")
+  			expect(array.new_array).to eq [nil, "cat", nil]
+  		end
+
+  		it "throws error when providing error out of range" do
+  			expect {array.set(3, "dog")}.to raise_error(StandardError)
+  		end
+  	end
+
+  	describe "#get" do
+  		it "retrieves element at index" do
+  			array.set(2, "dog")
+  			expect(array.get(2)).to eq "dog"
+  		end
+
+  		it "throws error when index out of range" do
+  			expect {array.get(3)}.to raise_error(StandardError)
+  		end
+  	end
 
 end
